150344103efd65c3a89c9874618e3d8665c67d9f
LuisRod1990 <luisrodj1990@gmail.com>
2026-02-05 09:08:21 -0600

    Actualización - Browser + Server

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,6 @@ export const environment = {
   production: true,
   authApi: 'http://ec2-3-142-208-223.us-east-2.compute.amazonaws.com:5000/api/auth',
   dataApi: 'http://ec2-3-142-208-223.us-east-2.compute.amazonaws.com:5001/api/Portfolio',
-  //authApi: 'https://localhost:44339/api/auth',
-  //dataApi: 'https://localhost:44347/api/Portfolio',
   endpoints: {
     login: '/login',
     refresh: '/refresh',
